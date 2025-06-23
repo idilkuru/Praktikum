0120510c82538977ac181f57cc50b41c97c7f656
faisal-ai <faisal.lambo@gmail.com>
2025-06-23 22:32:29 +0200

    added the results for different llms

--- evaluation/evaluation_llama.py
+++ evaluation/evaluation_llama.py
@@ -3,11 +3,11 @@ from collections import Counter
 import json
 
 # Load original benchmark file (ground truth)
-with open("../Data/merged_dataset_500.jsonl", "r", encoding="utf-8") as f:
+with open("../Data/merged_dataset_100.jsonl", "r", encoding="utf-8") as f:
     gold_data = {entry["id"]: entry for entry in map(json.loads, f)}
 
 # Load predictions file
-with open("../Data/llama3_merged_dataset_500_2.jsonl", "r", encoding="utf-8") as f:
+with open("../Data/qwen3_merged_dataset_100_5.jsonl", "r", encoding="utf-8") as f:
     pred_data = [json.loads(line) for line in f]
 
 gold_labels = []
--- ollama/model_runner.py
+++ ollama/model_runner.py
@@ -1,7 +1,7 @@
 # Sends prompt to Ollama and gets response
 import requests
 
-def query_llm(prompt, model="llama3"):
+def query_llm(prompt, model="qwen3:8b"): # llama3, qwen3:8b, deepseek-r1:8b, mistral:7b, command-r7b:7b, phi3:3.8b
     response = requests.post("http://localhost:11434/api/generate", json={
         "model": model,
         "prompt": prompt,
